cb32a027d415edafb4d8bc54e953b57ab2536a63
Agenc0 <tangentskate174@gmail.com>
2024-01-15 10:44:22 -0800

    corrected missing pointer indicator in function definition

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -9,7 +9,7 @@
  * Return: new node
  */
 
-binary_tree_t binary_tree_node(binary_tree_t *parent, int value)
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
         binary_tree_t *new;
 
