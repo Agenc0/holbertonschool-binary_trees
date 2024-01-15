3b94c039279592a4161118de264bf5a136dcfee2
Agenc0 <tangentskate174@gmail.com>
2024-01-15 15:45:18 -0800

    added t17 file

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_sibling - finds the sibling of a given node
+ * in a binary tree
+ * 
+ * @node: node to find sibling of
+ *
+ * Return: sibling of node if successful, NULL if else
+ */
+
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+	if (node->parent->left == node)
+		return (node->parent->right);
+	return (node->parent->left);
+}
