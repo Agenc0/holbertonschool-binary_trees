2cd2b59062b1b93297dca76381972b509b4fe6c1
Agenc0 <tangentskate174@gmail.com>
2024-01-15 13:35:07 -0800

    fixed t9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -14,8 +14,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	{
 		size_t l_height = 0, r_height = 0;
 
-		l_height = tree->left ? binary_tree_height(tree->left) : 0;
-		r_height = tree->right ? binary_tree_height(tree->right) : 0;
+		l_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		r_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 		return ((l_height > r_height) ? l_height : r_height);
 	}
 
