6dbbe2792d49f8e6ab813f4d2df1a84cf9dcddf3
Agenc0 <tangentskate174@gmail.com>
2024-01-15 14:57:00 -0800

    added t15 file

---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,38 @@
+#include "binary_trees.h"
+
+/**
+ * full_check - checks if a binary tree is full
+ *
+ * @tree: binary tree to check
+ *
+ * Return: 1 if full, 0 if not
+ */
+
+int full_check(const binary_tree_t *tree)
+{
+	if (tree != NULL)
+	{
+		if ((tree->left != NULL && tree->right == NULL) ||
+			tree->left == NULL && tree->right != NULL ||
+			full_check(tree->left) == 0 ||
+			full_check(tree->right) == 0)
+
+			return (0);
+	}
+	return (1);
+}
+
+/**
+ * binary_tree_is_full - checks if a binary tree is full
+ *
+ * @tree: binary tree to check
+ *
+ * Return: 1 if full, 0 if not full or tree is NULL
+ */
+
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	return (full_check(tree));
+}
